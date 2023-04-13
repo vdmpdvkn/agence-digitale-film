6af6e45fe641485e83b0ba89fdecca5147192089
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-04-13 19:00:23 +0300

    removed import line

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
 import { refs } from './modules/refs';
 import { onClickMyLibraryButton } from './modules/button-my-library';
 import { onClickHomeButton } from './modules/button-home';
-import './modules/film-info-api';
-
