d1f8be25787ad6d3f1cc5526b5300e7fad142bde
annatill <113561337+annatill@users.noreply.github.com>
2023-04-13 18:47:36 +0200

    Revert "add button throttle"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-import './modules/film-info-api';
-import headerMyLibraryButton from './modules/headerMyLibraryButton';
 import { refs } from './modules/refs';
 import { onClickMyLibraryButton } from './modules/button-my-library';
 import { onClickHomeButton } from './modules/button-home';
