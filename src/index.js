ae0573cd97487e062c12b60f75d6cfeacc61365c
Siracenco Serghei <siracencoserghei@gmail.com>
2023-04-14 22:17:33 +0200

    add toggle for modal buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { refs } from './modules/refs';
 import { onClickMyLibraryButton } from './modules/button-my-library';
 import { onClickHomeButton } from './modules/button-home';
+
+
+import {toggleWatchedBtn, toggleQueueBtn, toggleButtonClass} from './modules/localStorageWatchQueue/localStorageWatchedQueue';
