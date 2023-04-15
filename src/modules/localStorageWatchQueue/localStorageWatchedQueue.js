6e6c10cc57f6a2fe04038525c647de63eeb4cc3c
Siracenco Serghei <siracencoserghei@gmail.com>
2023-04-15 21:57:53 +0200

    add array

--- src/modules/localStorageWatchQueue/localStorageWatchedQueue.js
+++ src/modules/localStorageWatchQueue/localStorageWatchedQueue.js
@@ -5,7 +5,6 @@ import {
   } from '../localStorage/localStorage';
   import { refs } from '../refs';
   const { watchedBtnRef, queueBtnRef } = refs;
-  
   function toggleWatchedBtn() {
   if (watchedBtnRef.classList.contains('add-watched')) {
   watchedBtnRef.classList.remove('add-watched');
@@ -21,7 +20,6 @@ import {
   queueBtnRef.disabled = false;
   }
   }
-  
   function toggleQueueBtn() {
   if (queueBtnRef.classList.contains('add-queue')) {
   queueBtnRef.classList.remove('add-queue');
@@ -37,7 +35,6 @@ import {
   watchedBtnRef.disabled = false;
   }
   }
-  
   export function handleWatchedClick(event) {
   const button = event.target;
   const trailerId = button.dataset.id;
@@ -49,7 +46,7 @@ import {
   const genreIdsString = document.getElementById('js-watched').getAttribute('data-genre_ids');
   const genreIds = genreIdsString ? genreIdsString.split(' ').map(id => parseInt(id)) : [12];
   const releaseDate = document.getElementById('js-watched').getAttribute('data-release_date');
-  const watchedMovies = getStorage('watched');
+  const watchedMovies = getStorage(refs.WATCHED);
   if (button.classList.contains('add-watched')) {
   const filmInfo = {
   id: trailerId,
@@ -58,10 +55,11 @@ import {
   genre_ids: genreIds,
   release_date: releaseDate,
   };
-  watchedMovies.push(filmInfo);
-  setStorage('watched', watchedMovies);
+  // watchedMovies.push(filmInfo);
+  // setStorage(refs.WATCHED, watchedMovies);
+  setStorage(refs.WATCHED, filmInfo);
   } else if (button.classList.contains('remove-watched')) {
-  delFromStorage('watched', trailerId);
+  delFromStorage(refs.WATCHED, trailerId);
   }
   toggleWatchedBtn();
   }
@@ -75,7 +73,7 @@ import {
   const posterPath = fullPosterPath.slice(fullPosterPath.lastIndexOf('/') + 0);
   const genreIdsString = document.getElementById('js-queue').getAttribute('data-genre_ids');
   const genreIds = genreIdsString ? genreIdsString.split(' ').map(id => parseInt(id)) : [12];
-  const queuedMovies = getStorage('queue');
+  const queuedMovies = getStorage(refs.QUEUE);
   if (button.classList.contains('add-queue')) {
   const filmInfo = {
   id: trailerId,
@@ -84,10 +82,11 @@ import {
   genre_ids: genreIds,
   popularity: popularity,
   };
-  queuedMovies.push(filmInfo);
-  setStorage('queue', queuedMovies);
+  // queuedMovies.push(filmInfo);
+  // setStorage(refs.QUEUE, queuedMovies);
+  setStorage(refs.QUEUE, filmInfo);
   } else if (button.classList.contains('remove-queue')) {
-  delFromStorage('queue', trailerId);
+  delFromStorage(refs.QUEUE, trailerId);
   }
   toggleQueueBtn();
   }
