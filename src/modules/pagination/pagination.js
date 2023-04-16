12addc10da031ea19e1eb6cc21ceb5aae0323a45
Nepalcem <mark.emailtest@gmail.com>
2023-04-16 22:01:30 +0300

    pagination works!

--- src/modules/pagination/pagination.js
+++ src/modules/pagination/pagination.js
@@ -3,10 +3,11 @@ import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 import renderMoviesList from '../html-render';
 import { BASE_URL, API_KEY } from '../api-service';
+import fetchApi, { apiRefs } from '../api-service';
 
 // let paginationActive = false;
 
-const container = refs.paginationRef;
+// const container = refs.paginationRef;
 const searchQueryParameter = 'ukraine';
 
 const searchQueryOptions = {
@@ -15,32 +16,33 @@ const searchQueryOptions = {
   totalPages: 7,
 };
 
-const pagination = new Pagination(container, {
-  totalItems: searchQueryOptions.totalPages * 20, // Assuming 20 items per page
-  itemsPerPage: 20, // Number of items to display per page
-  visiblePages: 5, // Number of visible pages in the pagination
-  currentPage: searchQueryOptions.page, // Current page
-  centerAlign: true,
-});
+// const pagination = new Pagination(container, {
+//   totalItems: searchQueryOptions.totalPages * 20, // Assuming 20 items per page
+//   itemsPerPage: 20, // Number of items to display per page
+//   visiblePages: 5, // Number of visible pages in the pagination
+//   currentPage: searchQueryOptions.page, // Current page
+//   centerAlign: true,
+// });
 
-pagination.on('afterMove', eventData => {
-  searchQueryOptions.page = eventData.page;
+// pagination.on('afterMove', eventData => {
+//   searchQueryOptions.page = eventData.page;
 
-  performMovieSearch();
-});
+//   performMovieSearch();
+// });
 
-function performMovieSearch() {
-  const searchURL = `${searchQueryOptions.searchURL}&page=${searchQueryOptions.page}`;
-  fetch(searchURL)
-    .then(response => response.json())
-    .then(data => {
-      // Update the UI with the movie search results
-      renderMoviesList(data.results);
-    })
-    .catch(error => {
-      console.error('Error performing movie search:', error);
-    });
-}
+// function performMovieSearch() {
+//   const searchURL = `${searchQueryOptions.searchURL}&page=${searchQueryOptions.page}`;
+//   fetch(searchURL)
+//     .then(response => response.json())
+//     .then(data => {
+//       // Update the UI with the movie search results
+//       renderMoviesList(data.results);
+//     })
+//     .catch(error => {
+//       console.error('Error performing movie search:', error);
+//     });
+// }
+
 // if (paginationActive) {
 //   const pagination = new Pagination(container, options);
 // } else {
--- src/modules/search/fetch.js
+++ src/modules/search/fetch.js
@@ -1,17 +1,26 @@
 import { refs } from '../refs';
 import { Notify } from 'notiflix';
-import fetchApi from '../api-service';
+import fetchApi, { apiRefs } from '../api-service';
 import { apiRefs } from '../api-service';
 import renderMoviesList from '../html-render';
-
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
+import { BASE_URL, API_KEY } from '../api-service';
 
 // ref.alertMessage = document.querySelector('.header-form--search'),
 refs.formRef.addEventListener('submit', fetchFilmOfSearch);
 
+let pagination;
+
 export async function fetchFilmOfSearch(e) {
   e.preventDefault();
+
   const searchQuery = e.currentTarget.elements.film.value.trim();
-  e.currentTarget.reset(); 
+  e.currentTarget.reset();
+
+  if (pagination) {
+    pagination.reset();
+  }
 
   const data = await fetchApi({
     param: apiRefs.SEARCH,
@@ -22,27 +31,57 @@ export async function fetchFilmOfSearch(e) {
   console.log(dataEl);
   console.log(data);
 
+  if (searchQuery === '') {
+    refs.alertMessage.style.display = 'none';
+    Notify.warning('Searching starts after providing data to search!');
+    return;
+  } else if (dataEl === 0) {
+    // refs.alertMessage.classList.add('visually-hidden');
+    refs.alertMessage.style.display = 'flex';
+    Notify.warning(
+      'Search result not successful. Enter the correct movie name!'
+    );
+    return;
+  } else {
+    refs.alertMessage.style.display = 'none';
+    Notify.success(` We found ${data.total_results} films.`);
+  }
+  renderMoviesList(data.results);
 
-if (searchQuery === '') {
-  refs.alertMessage.style.display = 'none';
-  Notify.warning('Searching starts after providing data to search!');
-  return;
-} else if (dataEl === 0) {
-  // refs.alertMessage.classList.add('visually-hidden');
-  refs.alertMessage.style.display = 'flex';
-  Notify.warning('Search result not successful. Enter the correct movie name!');
-  return;
+  const paginationParams = {
+    totalItems: data.total_results,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    currentPage: 1,
+    centerAlign: true,
+  };
 
-} else {
-  refs.alertMessage.style.display = 'none';
-  Notify.success(` We found ${data.total_results} films.`);
+  pagination = new Pagination(refs.paginationRef, paginationParams);
+
+  pagination.on('afterMove', eventData => {
+    paginationParams.currentPage = eventData.page;
+
+    performMovieSearch();
+  });
+
+  function performMovieSearch() {
+    fetchApi({
+        param: apiRefs.SEARCH,
+        page: paginationParams.currentPage,
+        query: searchQuery,
+      })
+    // const searchURL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${paginationParams.currentPage}`;
+    // fetch(searchURL)
+      // .then(response => response.json())
+      .then(data => {
+        // Update the UI with the movie search results
+        renderMoviesList(data.results);
+      })
+      .catch(error => {
+        console.error('Error performing movie search:', error);
+      });
+  }
 }
-renderMoviesList(data.results);
-}
-
-
-
-
 
 // refs.formRef.addEventListener('submit', fetchFilmOfSearch);
 
