71a8aedffdf559de8ddb26fc645e1050ef8be204
Nepalcem <mark.emailtest@gmail.com>
2023-04-16 22:47:32 +0300

    update#2

--- src/modules/search/fetch.js
+++ src/modules/search/fetch.js
@@ -2,16 +2,24 @@ import { refs } from '../refs';
 import fetchApi from '../api-service';
 import { apiRefs } from '../api-service';
 import renderMoviesList from '../html-render';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 refs.formRef.addEventListener('submit', fetchFilmOfSearch);
 
 export function errorSearch() {
   refs.alertMessage.style.display = 'none';
 }
- 
+
+let pagination;
+
 export async function fetchFilmOfSearch(e) {
   e.preventDefault();
   const searchQuery = e.currentTarget.elements.film.value.trim();
+
+  if (pagination) {
+    pagination.reset();
+  }
 
   const data = await fetchApi({
     param: apiRefs.SEARCH,
@@ -22,30 +30,58 @@ export async function fetchFilmOfSearch(e) {
   // console.log(dataEl);
   // console.log(data);
 
-if (searchQuery === '') {
+  if (searchQuery === '') {
     refs.alertMessage.textContent =
       'Searching starts after providing data to search!';
     refs.alertMessage.style.display = 'flex';
     return;
-} else if (dataEl === 0) {
+  } else if (dataEl === 0) {
     refs.alertMessage.textContent =
       'Search result not successful. Enter the correct movie name!';
     refs.alertMessage.style.display = 'flex';
     return;
-} else {
-    refs.alertMessage.textContent =
-      'Successful!';
+  } else {
+    refs.alertMessage.textContent = 'Successful!';
     refs.alertMessage.style.display = 'flex';
+  }
+  refs.errorSearchRef.addEventListener('input', errorSearch);
 
+  renderMoviesList(data.results);
+
+  const paginationParams = {
+    totalItems: data.total_results,
+    itemsPerPage: 20,
+    visiblePages: 5,
+    currentPage: 1,
+    centerAlign: true,
+  };
+
+  pagination = new Pagination(refs.paginationRef, paginationParams);
+
+  pagination.on('afterMove', eventData => {
+    paginationParams.currentPage = eventData.page;
+
+    performMovieSearch();
+  });
+
+  function performMovieSearch() {
+    fetchApi({
+      param: apiRefs.SEARCH,
+      page: paginationParams.currentPage,
+      query: searchQuery,
+    })
+      // const searchURL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${paginationParams.currentPage}`;
+      // fetch(searchURL)
+      // .then(response => response.json())
+      .then(data => {
+        // Update the UI with the movie search results
+        renderMoviesList(data.results);
+      })
+      .catch(error => {
+        console.error('Error performing movie search:', error);
+      });
+  }
 }
-refs.errorSearchRef.addEventListener('input', (errorSearch));
-
-renderMoviesList(data.results);
-}
-
-
-
-
 
 // refs.formRef.addEventListener('submit', fetchFilmOfSearch);
 
@@ -64,9 +100,6 @@ renderMoviesList(data.results);
 //   renderMoviesList(data.results);
 
 // }
-
-
-
 
 // export function errorSearch() {
 //   refs.alertMessage.style.display = 'none';
