ce84e7c035f3634d30df71112d290c0ff047f050
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-04-16 16:34:57 +0300

    initial commit

--- src/modules/film-info/openFilmInfoOnPosterClick.js
+++ src/modules/film-info/openFilmInfoOnPosterClick.js
@@ -3,34 +3,41 @@ import fetchApi from '../api-service';
 import { apiRefs } from '../api-service';
 import { renderFilmInfo } from './renderFilmInfo';
 import {
-closeFilmInfoOnBackdropClick,
-closeFilmInfoOnEsc,
-closeFilmInfoOnCloseBtnClick,
+  closeFilmInfoOnBackdropClick,
+  closeFilmInfoOnEsc,
+  closeFilmInfoOnCloseBtnClick,
 } from './closeFilmInfoModal';
-import {handleFilmInfoData} from './handelFilmInfoData';
-
+import { handleFilmInfoData } from './handelFilmInfoData';
 
 export function openFilmInfoOnPosterClick(evt) {
-if (evt.target.nodeName !== 'LI' && evt.target.parentNode.nodeName !== 'LI') {
-return;
+  if (
+    evt.target.nodeName !== 'LI' &&
+    evt.target.parentNode.nodeName !== 'LI' &&
+    evt.target.parentNode.parentNode.nodeName !== 'LI'
+  ) {
+    return;
+  }
+
+  const { backdropRef, filmInfoCloseBtnRef, watchedBtnRef, queueBtnRef } = refs;
+
+  fetchApi(
+    apiRefs.MOVIE_DETAILS,
+    Number(
+      evt.target.dataset.id ??
+        evt.target.parentNode.dataset.id ??
+        evt.target.parentNode.parentNode.dataset.id
+    )
+  )
+    .then(data => {
+      handleFilmInfoData(data, watchedBtnRef, queueBtnRef);
+      renderFilmInfo(data);
+    })
+    .then(() => {
+      backdropRef.classList.remove('is-hidden');
+    });
+
+  document.body.style.overflow = 'hidden';
+  document.addEventListener('click', closeFilmInfoOnEsc);
+  backdropRef.addEventListener('click', closeFilmInfoOnBackdropClick);
+  filmInfoCloseBtnRef.addEventListener('click', closeFilmInfoOnCloseBtnClick);
 }
-
-const { backdropRef, filmInfoCloseBtnRef, watchedBtnRef, queueBtnRef } = refs;
-
-fetchApi(
-apiRefs.MOVIE_DETAILS,
-Number(evt.target.dataset.id ?? evt.target.parentNode.dataset.id)
-)
-.then(data => {
-handleFilmInfoData(data, watchedBtnRef, queueBtnRef);
-renderFilmInfo(data);
-})
-.then(() => {
-backdropRef.classList.remove('is-hidden');
-});
-
-document.body.style.overflow = 'hidden';
-backdropRef.addEventListener('click', closeFilmInfoOnBackdropClick);
-filmInfoCloseBtnRef.addEventListener('click', closeFilmInfoOnCloseBtnClick);
-}
-
