5da5ae7d226855ef1c98295c53ff5a1f9ab6ed48
annatill <anna.tataurova.95@gmail.com>
2023-04-17 18:36:05 +0200

    pagination my library

--- src/modules/header/button-queue.js
+++ src/modules/header/button-queue.js
@@ -5,13 +5,34 @@ import {
   delFromStorage,
   clearStorage,
   getItemFromStorage,
+  getStorageLength,
 } from '../localStorage/localStorage';
 
 import renderMoviesList from '../html-render';
 
+import Pagination from 'tui-pagination';
+
 export function onClickButtonQueue() {
   refs.buttonRefQueue.classList.add('header-button__library--active');
   refs.buttonRefWatched.classList.remove('header-button__library--active');
-  const queue = getStorage(refs.QUEUE);
+  const pagination = new Pagination(refs.paginationRef, {
+    totalItems: getStorageLength(refs.QUEUE),
+    itemsPerPage: 5,
+    visiblePages: 5,
+    currentPage: 1,
+    centerAlign: true,
+  });
+
+  pagination.on('afterMove', eventData => {
+    const page = eventData.page;
+
+    const queue = getStorage(refs.QUEUE, {
+      page,
+      perPage: 5,
+    });
+    renderMoviesList(queue);
+  });
+
+  const queue = getStorage(refs.QUEUE, { page: 1, perPage: 5 });
   renderMoviesList(queue);
 }
