99bcab2db01bcc130c955f17f08b4066fe405bf7
annatill <anna.tataurova.95@gmail.com>
2023-04-17 19:50:38 +0200

    add pagination

--- src/modules/header/button-my-library.js
+++ src/modules/header/button-my-library.js
@@ -1,40 +1,12 @@
 import { refs } from '../refs';
-import {
-  setStorage,
-  getStorage,
-  delFromStorage,
-  clearStorage,
-  getItemFromStorage,
-  getStorageLength,
-} from '../localStorage/localStorage';
+import { paginationStorage } from '../pagination/pagination';
 
-import renderMoviesList from '../html-render';
-import Pagination from 'tui-pagination';
+let paginationMyLibrary;
 
 export function onClickMyLibraryButton() {
   refs.divRefButtonLibrary.classList.add('header-logo__library--position');
   refs.buttonRefLibrary.classList.add('header-nav--active');
   refs.buttonRefHome.classList.remove('header-nav--active');
   refs.divRefWatchedQueue.style.display = 'flex';
-
-  const pagination = new Pagination(refs.paginationRef, {
-    totalItems: getStorageLength(refs.QUEUE),
-    itemsPerPage: 5,
-    visiblePages: 5,
-    currentPage: 1,
-    centerAlign: true,
-  });
-
-  pagination.on('afterMove', eventData => {
-    const page = eventData.page;
-
-    const queue = getStorage(refs.QUEUE, {
-      page,
-      perPage: 5,
-    });
-    renderMoviesList(queue);
-  });
-
-  const queue = getStorage(refs.QUEUE, { page: 1, perPage: 5 });
-  renderMoviesList(queue);
+  paginationStorage(refs.QUEUE, paginationMyLibrary);
 }
