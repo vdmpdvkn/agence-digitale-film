779968c6fa6098689a570173ad65d4621e94d7a4
Andrew Ketsko <monicman82@gmail.com>
2023-04-17 18:31:05 +0300

    search pagination ghanged

--- src/modules/search/fetch.js
+++ src/modules/search/fetch.js
@@ -3,6 +3,7 @@ import fetchApi from '../api-service';
 import { apiRefs } from '../api-service';
 import renderMoviesList from '../html-render';
 import Pagination from 'tui-pagination';
+import { paginationFunc } from '../pagination/pagination';
 
 refs.formRef.addEventListener('submit', fetchFilmOfSearch);
 refs.errorSearchRef.addEventListener('input', errorSearch);
@@ -16,10 +17,6 @@ let pagination;
 export async function fetchFilmOfSearch(e) {
   e.preventDefault();
   const searchQuery = e.currentTarget.elements.film.value.trim();
-
-  if (pagination) {
-    pagination.reset();
-  }
 
   const data = await fetchApi({
     param: apiRefs.SEARCH,
@@ -47,38 +44,48 @@ export async function fetchFilmOfSearch(e) {
   }
 
   renderMoviesList(data.results);
+  console.log(data.total_results);
 
-  const paginationParams = {
-    totalItems: data.total_results,
-    itemsPerPage: 20,
-    visiblePages: 5,
-    currentPage: 1,
-    centerAlign: true,
-  };
-
-  pagination = new Pagination(refs.paginationRef, paginationParams);
-
-  pagination.on('afterMove', eventData => {
-    paginationParams.currentPage = eventData.page;
-
-    performMovieSearch();
+  paginationFunc(data.total_results, {
+    param: apiRefs.SEARCH,
+    query: searchQuery,
   });
 
-  function performMovieSearch() {
-    fetchApi({
-      param: apiRefs.SEARCH,
-      page: paginationParams.currentPage,
-      query: searchQuery,
-    })
-      // const searchURL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${paginationParams.currentPage}`;
-      // fetch(searchURL)
-      // .then(response => response.json())
-      .then(data => {
-        // Update the UI with the movie search results
-        renderMoviesList(data.results);
-      })
-      .catch(error => {
-        console.error('Error performing movie search:', error);
-      });
-  }
+  //   if (pagination) {
+  //     pagination.reset();
+  //   }
+
+  // const paginationParams = {
+  //   totalItems: data.total_results,
+  //   itemsPerPage: 20,
+  //   visiblePages: 5,
+  //   currentPage: 1,
+  //   centerAlign: true,
+  // };
+
+  // pagination = new Pagination(refs.paginationRef, paginationParams);
+
+  // pagination.on('afterMove', eventData => {
+  //   paginationParams.currentPage = eventData.page;
+
+  //   performMovieSearch();
+  // });
+
+  //   function performMovieSearch() {
+  //     fetchApi({
+  //       param: apiRefs.SEARCH,
+  //       page: paginationParams.currentPage,
+  //       query: searchQuery,
+  //     })
+  //       // const searchURL = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchQuery}&page=${paginationParams.currentPage}`;
+  //       // fetch(searchURL)
+  //       // .then(response => response.json())
+  //       .then(data => {
+  //         // Update the UI with the movie search results
+  //         renderMoviesList(data.results);
+  //       })
+  //       .catch(error => {
+  //         console.error('Error performing movie search:', error);
+  //       });
+  //   }
 }
