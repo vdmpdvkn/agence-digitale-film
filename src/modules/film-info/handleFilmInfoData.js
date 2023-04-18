92d1a33922776a191b17e7e220961e872c87086e
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-04-18 20:04:43 +0300

    classlist toggle update

--- src/modules/film-info/handleFilmInfoData.js
+++ src/modules/film-info/handleFilmInfoData.js
@@ -19,19 +19,24 @@ export function handleFilmInfoData(data, watchedBtnRef, queueBtnRef) {
     watchedBtnRef.classList.remove('add-watched');
     watchedBtnRef.classList.add('remove-watched');
     watchedBtnRef.textContent = 'Remove from Watched';
+    watchedBtnRef.classList.add('buttons-list__button--black');
   } else {
     watchedBtnRef.classList.remove('remove-watched');
     watchedBtnRef.classList.add('add-watched');
     watchedBtnRef.textContent = 'Add to Watched';
+    watchedBtnRef.classList.remove('buttons-list__button--black');
   }
   if (!!itemInQueue) {
     queueBtnRef.classList.remove('add-queue');
     queueBtnRef.classList.add('remove-queue');
     queueBtnRef.textContent = 'Remove from Queue';
+    queueBtnRef.classList.add('buttons-list__button--black');
+    console.log(queueBtnRef);
   } else {
     queueBtnRef.classList.remove('remove-queue');
     queueBtnRef.classList.add('add-queue');
     queueBtnRef.textContent = 'Add to Queue';
+    queueBtnRef.classList.remove('buttons-list__button--black');
   }
   watchedBtnRef.addEventListener('click', handleWatchedClick);
   queueBtnRef.addEventListener('click', handleQueueClick);
